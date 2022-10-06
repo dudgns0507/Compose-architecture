49908eb90ec0144b48ae494988fbcb0e3f8c17a4
copin-danny <hoon2585@gmail.com>
2022-10-06 18:25:05 +0900

    feature - lazy

--- app/src/main/java/com/github/dudgns0507/compose/sample/MainActivity.kt
+++ app/src/main/java/com/github/dudgns0507/compose/sample/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
@@ -48,9 +50,9 @@ fun MyApp(names: List<String> = listOf("Android", "Compose")) {
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
@@ -107,6 +109,6 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
 @Composable
 fun DefaultPreview() {
     ComposeSampleTheme {
-        OnboardingScreen(onContinueClicked = {})
+        Greetings()
     }
 }
