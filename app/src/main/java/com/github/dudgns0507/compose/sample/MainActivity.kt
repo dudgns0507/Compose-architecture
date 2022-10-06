10ad3dc5c8222df1adc5825bc343315e70a04f97
copin-danny <hoon2585@gmail.com>
2022-10-06 17:47:00 +0900

    feature - reuse composable

--- app/src/main/java/com/github/dudgns0507/compose/sample/MainActivity.kt
+++ app/src/main/java/com/github/dudgns0507/compose/sample/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.dudgns0507.compose.sample.ui.theme.ComposeSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +19,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeSampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
+fun MyApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Greeting("Android")
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.background) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
     }
 }
 
@@ -40,6 +46,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     ComposeSampleTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
